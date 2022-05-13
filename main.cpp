041a293b916e885b5a22690fe9ab7fe50d308021
John <johnp.patt@gmail.com>
2022-05-13 12:51:53 -0700

    Added license notice

--- main.cpp
+++ main.cpp
@@ -1,3 +1,17 @@
+Copyright 2018 John-TC https://github.com/john-TC
+
+This program is free software: you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <https://www.gnu.org/licenses/>. 
+
 #include <Arduino.h>
 #include <Bounce2.h>
 #include <LiquidCrystal.h>
